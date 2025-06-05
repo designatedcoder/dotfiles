25435867a2cd4fc5b17510d79915b71d871fe45c
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2025-06-05 03:37:55 -0400

    cleaned up telescope config

--- nvim/lua/designatedcoder/plugins/telescope.lua
+++ nvim/lua/designatedcoder/plugins/telescope.lua
@@ -1,11 +1,11 @@
 return {
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
         dependencies = {
             "nvim-lua/plenary.nvim",
-            { 
+            {
                 "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make" 
+                build = "make"
             },
             "nvim-tree/nvim-web-devicons",
         },
